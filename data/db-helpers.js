736b44102e5c7518726816352f2fc37a6ea4a70e
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-13 13:34:18 -0500

    set up get and post requests

--- data/db-helpers.js
+++ data/db-helpers.js
@@ -4,21 +4,31 @@ module.exports = {
    getResources,
    getProjects,
    getTasks,
-   insertProject
+   insertProject,
+   insertResource,
+   insertTask
 }
 
 function getResources() {
-   
+   return db('resources');
 }
 
 function getProjects() {
-
+   return db('projects');
 }
 
 function getTasks() {
-
+   return db('tasks');
 }
 
-function insertProject() {
+function insertProject(data) {
+   return db('projects').insert(data, "id");
+}
 
+function insertResource(data) {
+   return db('resources').insert(data, "id");
+}
+
+function insertTask(data) {
+   return db('tasks').insert(data, "id");
 }
